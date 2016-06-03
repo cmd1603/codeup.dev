ce41eafe5af99ed3203caa83b0dc718a1c78749c
Chris Davila <cmd1603@gmail.com>
2016-06-03 16:47:52 -0500

    adding iterating arrays exercises

--- public/js/iterating_arrays.js
+++ public/js/iterating_arrays.js
@@ -1,14 +1,19 @@
 'use strict'
 
-			// todo:
-			// Create an array of 4 names using literal array notation, in a variable called 'names'.
+			
 			var names = ['Ashley', 'Valerie', 'Zelina', 'Diana'];
 			// todo:
 			// Create a log statement that will log the number of elements in the names array.
-			console.log("There are " + names.length + " names in the names array.");
-			// todo:
-			// Create log statements that will print each of the names array elements individually.
-			console.log("The first name is: " + names[0]);
-			console.log("The second name is: " + names[1]);
-			console.log("The thrid name is: " + names[2]);
-			console.log("The fourth name is: " + names[3]);		
+				console.log("There are " + names.length + " names in the names array.");
+			
+			for (var i = 0; i < names.length; i++) {
+				console.log('Names at index ' + i + ' is ' + names[i]);
+			}
+
+				console.log("ForEach loop:");
+			names.ForEach(function (element, index, array) {
+				console.log('')
+			}
+
+				console.log('Name at index ' + index + ' is ' + element);
+			});
