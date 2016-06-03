f50b5e8c0f3cd5c412588a6876f64d71cec22b19
Chris Davila <cmd1603@gmail.com>
2016-06-03 16:15:19 -0500

    adding first arrays exercise

---
+++ public/js/iterating_arrays.js
@@ -0,0 +1,14 @@
+'use strict'
+
+			// todo:
+			// Create an array of 4 names using literal array notation, in a variable called 'names'.
+			var names = ['Ashley', 'Valerie', 'Zelina', 'Diana'];
+			// todo:
+			// Create a log statement that will log the number of elements in the names array.
+			console.log("There are " + names.length + " names in the names array.");
+			// todo:
+			// Create log statements that will print each of the names array elements individually.
+			console.log("The first name is: " + names[0]);
+			console.log("The second name is: " + names[1]);
+			console.log("The thrid name is: " + names[2]);
+			console.log("The fourth name is: " + names[3]);		
