90151af4f9d783155333e6230feb3616b542d433
Chris Davila <cmd1603@gmail.com>
2016-08-03 15:23:22 -0500

    Saving switching seeder exercise to include bind value

--- park_seeder.php
+++ park_seeder.php
@@ -8,12 +8,11 @@ $query = "TRUNCATE national_parks";
 
 $dbc->exec($query);
 
-$query = "INSERT INTO national_parks (name,location,date_established,area_in_acres) VALUES ('Kings Canyon', 'California', '1940-03-24', 461901.20)";
-
-$numRows = $dbc->exec($query);
-echo "Inserted $numRows row." . PHP_EOL;
+// $numRows = $dbc->exec($query);
+// echo "Inserted $numRows row." . PHP_EOL;
 
 $national_parks = [
+	['name' => 'Kings Canyon', 'location' => 'California', 'date_established' => '1940-03-24', 'area_in_acres' => 461901.20 ],
 	['name' => 'Yellowstone', 'location' => 'Wyoming, Montana, Idaho', 'date_established' => '1872-03-01', 'area_in_acres' => 2219790.71 ],
 	['name' => 'Arches', 'location' => 'Utah', 'date_established' => '1929-04-12', 'area_in_acres' => 76518.98 ],
 	['name' => 'Badlands', 'location' => 'South Dakota', 'date_established' => '1978-11-10', 'area_in_acres' => 242755.94 ],
@@ -25,10 +24,15 @@ $national_parks = [
 	['name' => 'Capitol Reef', 'location' => 'Utah', 'date_established' => '1971-12-18', 'area_in_acres' => 241904.26 ]
 ];
 
-foreach ($national_parks as $national_park) {
-	$query = "INSERT INTO national_parks (name,location,date_established,area_in_acres) VALUES ('{$national_park['name']}', '{$national_park['location']}', '{$national_park['date_established']}', '{$national_park['area_in_acres']}')";
+$stmt = $dbc->prepare('INSERT INTO national_parks(name, location, date_established, area_in_acres) VALUES (:name, :location, :date_established, :area_in_acres)');
 
-$dbc->exec($query);
+foreach ($national_parks as $national_park) {
+	$stmt->bindValue(':name', $national_park['name'], PDO:: PARAM_STR);
+	$stmt->bindValue(':location', $national_park['location'], PDO:: PARAM_STR);
+	$stmt->bindValue(':date_established', $national_park['date_established'], PDO:: PARAM_STR);
+	$stmt->bindValue(':area_in_acres', $national_park['area_in_acres'], PDO:: PARAM_INT);
+
+$stmt->execute();
 
 echo "Inserted ID: " . $dbc->lastInsertId() . PHP_EOL;
 
