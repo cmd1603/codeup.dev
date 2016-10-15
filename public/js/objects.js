ba8f33de9f61d6607e0a61c1ab192242710415c7
Chris Davila <cmd1603@gmail.com>
2016-10-14 20:00:57 -0500

    Reworked objects exercise

--- public/js/objects.js
+++ public/js/objects.js
@@ -1,26 +1,21 @@
 (function(){
     "use strict";
 
-    // TODO: Create person object
-    var person = {};
-    	person.firstName = "Chris";
-    	person.lastName = "Davila";
-
-
-
-function sayHello (person) {
-	alert('Hello ' + person + '!');
-}
-
-
- 
-
-    // TODO: Create firstName and lastName properties in your person object; assign your name to them
-
+    // TODO: Create person object with firstName and lastName properties.
     // TODO: Add a sayHello method to the person object that
     //       alerts a greeting using the firstName and lastName properties
 
     // Say hello from the person object.
+    var person = {
+    	firstName: "Chris",
+    	lastName: "Davila",
+        sayHello: function() {
+            return this.firstName + ' ' + this.lastName;
+        }
+    };
 
-sayHello(person);
+    alert('Hello, ' + person.sayHello());
+
+    console.log(person.sayHello() + ' says hello!');
+
 })();
